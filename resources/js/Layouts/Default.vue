d725ab966b876e58014dbd445b6f957e500fe42f
Kevin Kirchner <kirchner.kevin@gmail.com>
2020-09-14 02:38:38 -0500

    give it a little padding for mobile's sake

--- resources/js/Layouts/Default.vue
+++ resources/js/Layouts/Default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="DefaultLayout leading-6 container mx-auto text-gray-900">
+  <div class="DefaultLayout leading-6 container mx-auto px-6 text-gray-900">
     <h1 class="text-4xl font-bold leading-10">
       grayscale <span class="text-gray-500">design</span>
     </h1>
