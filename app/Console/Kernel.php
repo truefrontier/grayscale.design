1783412931ab3a4fd434e6ebf6a49809d07bca27
Kevin Kirchner <kirchner.kevin@gmail.com>
2020-09-20 00:25:38 -0500

    phpfmt

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,8 +5,7 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
-class Kernel extends ConsoleKernel
-{
+class Kernel extends ConsoleKernel {
     /**
      * The Artisan commands provided by your application.
      *
@@ -32,9 +31,8 @@ class Kernel extends ConsoleKernel
      *
      * @return void
      */
-    protected function commands()
-    {
-        $this->load(__DIR__.'/Commands');
+    protected function commands() {
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
