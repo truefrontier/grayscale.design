d156422f4f343994c48f84e6ec4d19e9f686b188
Kevin Kirchner <kirchner.kevin@gmail.com>
2020-09-21 12:02:55 -0500

    only load scripts on prod

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,9 +8,11 @@
         <title>grayscale design | Luminance-based color palette generator for Tailwind CSS</title>
         <link rel="stylesheet" href="{{ mix('css/tailwind.css') }}" />
         <script src="https://kit.fontawesome.com/41590b8d21.js" crossorigin="anonymous"></script>
+        @if (env('APP_ENV') === 'production')
         <!-- Fathom - beautiful, simple website analytics -->
         <script src="https://cdn.usefathom.com/script.js" spa="auto" site="QBCMJQGI" defer></script>
         <!-- / Fathom -->
+        @endif
     </head>
     <body>
         @inertia
